6e7a99aee8e0f7a259b62b759d61553396a15603
chris <christopher.schmidt89@gmx.at>
2021-10-08 19:58:57 +0200

    datentypen initialisiert

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,27 @@
 public class calculator {
 
     public static void main(String[] args) {
+int a= 1;
+        System.out.println(a);
+
+        double b= 1.1;
+                System.out.println("Output double: \n " +b+ " das war der output bliblablub");
+        boolean c= false;
+
+        char d= 'd';
+
+        String e= "hallo";
+        System.out.println(c);
+
+        System.out.println(d);
+
+        System.out.println(d);
+
+
+
+
+
+
 
 
     }
